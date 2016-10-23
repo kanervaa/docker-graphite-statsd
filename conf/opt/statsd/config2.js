dec9f92e216469c231253d0a6f8c75c14fd72394
Jukka Kanerva-aho <jukka.kanerva-aho@kaltiot.com>
2016-10-23 20:44:32 +0300

    Daemons

--- conf/opt/statsd/config2.js
+++ conf/opt/statsd/config2.js
@@ -2,5 +2,6 @@
   "graphiteHost": "127.0.0.1",
   "graphitePort": 2003,
   "port": 8124,
+  "mgmt_port": 8127,
   "flushInterval": 10000
 }
--- conf/opt/statsd/config3.js
+++ conf/opt/statsd/config3.js
@@ -2,5 +2,6 @@
   "graphiteHost": "127.0.0.1",
   "graphitePort": 2003,
   "port": 8123,
+  "mgmt_port": 8128,
   "flushInterval": 10000
 }
